da3fd9ba28462cddd38488c1799818fba800f820
Xy90000001 <Xy90000001@gmail.com>
2023-07-08 23:37:39 -0800

    first commit

---
+++ botsel.py
@@ -0,0 +1,64 @@
+from seleniumwire import webdriver
+from selenium import webdriver as cdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# from chromedriver_py import binary_path
+from threading import Thread
+
+proxy_oxy = "http://Xy90000001:Xy90000001@unblock.oxylabs.io:60000"
+# proxy_oxy2 = "unblock.oxylabs.io:60000:Xy90000001:Xy90000001"
+url_adsite = "https://a000.ex16.repl.co/"
+url_ident = 'https://ident.me/'
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument('--no-sandbox')
+chromeOptions.add_argument('--disable-setuid-sandbox')
+# chromeOptions.add_argument('--disable-dev-shm-usage')
+chromeOptions.add_argument("--headless")
+# chromeOptions.add_argument("--remote-debugging-port=9222")  # this
+
+def wiredriver(PROXY=None):
+  seleniumwire_options = {
+    'proxy': {
+      'http': PROXY,
+      'verify_ssl': False,
+    },
+    'start-maximized': True,
+    'headless': True,
+  }
+  
+  # serv = Service(ChromeDriverManager().install())
+  # serv = Service(binary_path)
+  # serv = Service('chromium-browser')
+  driver = webdriver.Chrome(options=chromeOptions,seleniumwire_options=seleniumwire_options)
+#   driver = webdriver.Chrome(service=serv)
+  # driver = cdriver.Chrome(service=serv, options=chromeOptions)
+  return driver
+
+def mainloop():
+#   driver = wiredriver(proxy_oxy)
+  url_ad = "https://ad.a-ads.com/2199641?size=728x90"
+  driver = wiredriver()
+  # driver = cdriver.Chrome(options=chromeOptions)
+  driver.get(url_adsite)
+  # driver.get(url_ad)
+  # driver.get(url_ident)
+  # print(driver.page_source)
+#   for i in range(3):
+  n = 0
+  while True:
+    n+=1
+    print('refreshig...',n)
+    driver.refresh()
+    # driver.click()
+
+
+# import keepalive
+# keepalive.keep_alive()
+mainloop()
+
+# def botsel_thread():
+#   # t = threading.Thread(target=, args = (q,u))
+#   t = Thread(target=mainloop)
+#   t.start()
+#   #time.sleep(1)
